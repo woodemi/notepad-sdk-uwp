ea86659c4c6f8b94176129580abff1472bcd928b
wangkun <wangkun@woodemi.com>
2020-02-19 12:42:04 +0800

    WIP: Apply scale to absolute position
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- NotepadKitSample/MainPage.xaml.cs
+++ NotepadKitSample/MainPage.xaml.cs
@@ -4,10 +4,14 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reactive.Threading.Tasks;
 using System.Timers;
+using Windows.Foundation;
+using Windows.Graphics.Display;
 using Windows.UI.Input.Preview.Injection;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using NotepadKit;
+using Size = System.Drawing.Size;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -18,7 +22,7 @@ namespace NotepadKitSample
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private readonly InjectPenHelper _injectPenHelper = new InjectPenHelper(1.0);
+        private readonly InjectPenHelper _injectPenHelper = new InjectPenHelper(new Size(14800, 21000), 512);
         private readonly NotepadConnector _notepadConnector = new NotepadConnector();
         private readonly NotepadScanner _notepadScanner = new NotepadScanner();
         private readonly List<NotepadScanResult> _scanResultList = new List<NotepadScanResult>();
@@ -29,6 +33,14 @@ namespace NotepadKitSample
             InitializeComponent();
             _notepadScanner.Found += (sender, args) => { _scanResultList.Add(args); };
             _notepadConnector.ConnectionChanged += OnConnectionChanged;
+
+            var displayInformation = DisplayInformation.GetForCurrentView();
+            var scaleFactor = displayInformation.RawPixelsPerViewPixel;
+            var screenSize = new Size((int) displayInformation.ScreenWidthInRawPixels,
+                (int) displayInformation.ScreenHeightInRawPixels);
+            var appBounds = ApplicationView.GetForCurrentView().VisibleBounds;
+            var appSize = new Size((int) (appBounds.Width * scaleFactor), (int) (appBounds.Height * scaleFactor));
+            Debug.WriteLine($"scaleFactor {scaleFactor}, screenSize {screenSize}, appSize {appSize}");
         }
 
         private void OnConnectionChanged(NotepadClient sender, ConnectionState args)
@@ -118,15 +130,31 @@ namespace NotepadKitSample
 
         private readonly Timer _inRangeTimer;
 
+        private readonly int _maxPressure;
+
+        private readonly Rect _validBounds;
+
         private readonly double _viewScale;
 
-        private NotePenPointer? _lastNotePenPointer;
+        // private NotePenPointer? _lastNotePenPointer;
 
         private InjectedInputPenInfo _preInputPenInfo;
 
-        public InjectPenHelper(double viewScale)
+        public InjectPenHelper(Size padSize, int maxPressure)
         {
-            _viewScale = viewScale;
+            var displayInformation = DisplayInformation.GetForCurrentView();
+            var scaleFactor = displayInformation.RawPixelsPerViewPixel;
+            var screenSize = new Size((int) (displayInformation.ScreenWidthInRawPixels * scaleFactor),
+                (int) (displayInformation.ScreenHeightInRawPixels * scaleFactor));
+            _viewScale = Math.Max(screenSize.Width * 1.0 / padSize.Width, screenSize.Height * 1.0 / padSize.Height);
+            var widthPadding = padSize.Width * _viewScale - screenSize.Width;
+            var heightPadding = padSize.Height * _viewScale - screenSize.Height;
+            Debug.WriteLine($"_viewScale {_viewScale}, widthPadding {widthPadding}, heightPadding {heightPadding}");
+            _validBounds = new Rect(widthPadding / 2, heightPadding / 2, screenSize.Width, screenSize.Height);
+            Debug.WriteLine($"_validBounds {_validBounds}");
+
+            _maxPressure = maxPressure;
+
             _inRangeTimer = new Timer
             {
                 Interval = 500,
@@ -150,15 +178,20 @@ namespace NotepadKitSample
         {
             _inRangeTimer.Stop();
 
-            var deltaX = _lastNotePenPointer.HasValue ? pointer.x - _lastNotePenPointer.Value.x : 0;
-            var deltaY = _lastNotePenPointer.HasValue ? pointer.y - _lastNotePenPointer.Value.y : 0;
+            // var deltaX = _lastNotePenPointer.HasValue ? pointer.x - _lastNotePenPointer.Value.x : 0;
+            // var deltaY = _lastNotePenPointer.HasValue ? pointer.y - _lastNotePenPointer.Value.y : 0;
+            //
+            // var penInfo = ToInputPenInfo(deltaX, deltaY, pointer.p);
+            // var penInfo = ToInputPenInfo(pointer);
+            // Debug.WriteLine($"ToInputPenInfo {penInfo}");
 
-            var penInfo = ToInputPenInfo(deltaX, deltaY, pointer.p);
-            Debug.WriteLine($"ToInputPenInfo {penInfo}");
+            var penInfo = ToInputPenInfo2(pointer);
+            Debug.WriteLine($"ToInputPenInfo2 {penInfo}");
+
             _inputInjector.InjectPenInput(penInfo);
             _preInputPenInfo = penInfo;
 
-            _lastNotePenPointer = pointer;
+            // _lastNotePenPointer = pointer;
 
             _inRangeTimer.Start();
         }
@@ -166,27 +199,15 @@ namespace NotepadKitSample
         private void OnTimedEvent(object sender, ElapsedEventArgs args)
         {
             Debug.WriteLine($"OnTimedEvent {args.SignalTime}");
-            _lastNotePenPointer = null;
+            // _lastNotePenPointer = null;
             _preInputPenInfo = null;
         }
 
-        private InjectedInputPenInfo ToInputPenInfo(int deltaX, int deltaY, int p)
+        private InjectedInputPointerInfo ToInputPointerInfo(int x, int y, int p)
         {
-            var preX = _preInputPenInfo?.PointerInfo.PixelLocation.PositionX ?? 0;
-            var preY = _preInputPenInfo?.PointerInfo.PixelLocation.PositionY ?? 0;
             var pointerInfo = new InjectedInputPointerInfo
             {
-                PixelLocation = new InjectedInputPoint
-                {
-                    PositionX = (int) (preX + deltaX * _viewScale),
-                    PositionY = (int) (preY + deltaY * _viewScale)
-                }
-            };
-            var penInfo = new InjectedInputPenInfo
-            {
-                PenParameters = InjectedInputPenParameters.Pressure,
-                PointerInfo = pointerInfo,
-                Pressure = p / 512.0
+                PixelLocation = new InjectedInputPoint {PositionX = x, PositionY = y}
             };
 
             if (_preInputPenInfo == null)
@@ -204,8 +225,38 @@ namespace NotepadKitSample
                 pointerInfo.PointerOptions |=
                     InjectedInputPointerOptions.PointerUp | InjectedInputPointerOptions.InRange;
 
-            penInfo.PointerInfo = pointerInfo;
-            return penInfo;
+            return pointerInfo;
+        }
+
+        private InjectedInputPenInfo ToInputPenInfo(int deltaX, int deltaY, int p)
+        {
+            var preX = _preInputPenInfo?.PointerInfo.PixelLocation.PositionX ?? 0;
+            var preY = _preInputPenInfo?.PointerInfo.PixelLocation.PositionY ?? 0;
+            var x = (int) (preX + deltaX * _viewScale);
+            var y = (int) (preY + deltaY * _viewScale);
+            var pointerInfo = ToInputPointerInfo(x, y, p);
+
+            return new InjectedInputPenInfo
+            {
+                PenParameters = InjectedInputPenParameters.Pressure,
+                PointerInfo = pointerInfo,
+                Pressure = p / 512.0
+            };
+        }
+
+        private InjectedInputPenInfo ToInputPenInfo2(NotePenPointer penPointer)
+        {
+            var screenPoint = new Point(penPointer.x * _viewScale, penPointer.y * _viewScale);
+            var x = Math.Max(_validBounds.Left, Math.Min(screenPoint.X, _validBounds.Right)) - _validBounds.Left;
+            var y = Math.Max(_validBounds.Top, Math.Min(screenPoint.Y, _validBounds.Bottom)) - _validBounds.Top;
+            var pointerInfo = ToInputPointerInfo((int) x, (int) y, penPointer.p);
+
+            return new InjectedInputPenInfo
+            {
+                PenParameters = InjectedInputPenParameters.Pressure,
+                PointerInfo = pointerInfo,
+                Pressure = penPointer.p / _maxPressure
+            };
         }
     }
 }
